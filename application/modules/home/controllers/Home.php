024672291dc0bf56b42f1e1ec0030604eb1acbed
siriphonnot <siriphonnot@gmail.com>
2018-07-12 16:30:26 +0700

    Initial Template

---
+++ application/modules/home/controllers/Home.php
@@ -0,0 +1,10 @@
+<?php
+defined('BASEPATH') or exit('No direct script access allowed');
+
+class Home extends MY_Controller
+{
+    public function index()
+    {
+        $this->load->view('index');
+    }
+}
